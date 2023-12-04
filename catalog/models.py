297bbf29e20a09050b3525b370d441f843cf7bd7
Artem <deusestmaleficus@gmail.com>
2023-12-04 23:00:03 +0700

    add models in models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+from django.db import models, connection
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True,
+}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Категория')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    @classmethod
+    def truncate_table_restart_id(cls):
+        with connection.cursor() as cursor:
+            cursor.execute(f'TRUNCATE TABLE {cls._meta.db_table} RESTART IDENTITY CASCADE')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    img = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    cost = models.DecimalField(verbose_name='цена')
+    first_date = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
+    last_date = models.DateTimeField(verbose_name='Дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}: {self.cost}.\n{self.description}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('name',)
