f9b51addad2180b783fb5b5b2954d537c1a81f4b
Artem <deusestmaleficus@gmail.com>
2023-12-06 22:31:47 +0700

    upgrade admin.py, models.py and requirements.txt

--- catalog/models.py
+++ catalog/models.py
@@ -7,15 +7,17 @@ NULLABLE = {
 
 
 class Category(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     category_name = models.CharField(max_length=50, verbose_name='Категория')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
-        return self.category_name
+        return f'{self.id}; {self.category_name}'
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ('category_name',)
 
     @classmethod
     def truncate_table_restart_id(cls):
@@ -24,6 +26,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     img = models.ImageField(upload_to='product/', verbose_name='Изображение', **NULLABLE)
@@ -31,9 +34,10 @@ class Product(models.Model):
     cost = models.IntegerField(verbose_name='Цена')
     first_date = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
     last_date = models.DateTimeField(verbose_name='Дата изменения', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='В наличии')
 
     def __str__(self):
-        return f'{self.name}: {self.cost}.\n{self.description}'
+        return f'{self.id}; {self.name}: {self.cost}, {self.category}'
 
     class Meta:
         verbose_name = 'товар'
